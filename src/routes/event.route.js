914961449fb454a2a1e03b3ac4ab57dc9e8faedd
billyAlton <israelbogninou@gmail.com>
2025-10-17 00:03:33 +0100

    sdf

--- src/routes/event.route.js
+++ src/routes/event.route.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const eventController = require("../controllers/event.controller");
 
 // CRUD
-router.post("/", eventController.createEvent);
-router.get("/", eventController.getEvents);
-router.get("/:id", eventController.getEventById);
-router.put("/:id", eventController.updateEvent);
-router.delete("/:id", eventController.deleteEvent);
+router.post("/create", eventController.createEvent);
+router.get("/get", eventController.getEvents);
+router.get("/getone/:id", eventController.getEventById);
+router.put("/update/:id", eventController.updateEvent);
+router.delete("/delete/:id", eventController.deleteEvent);
 
 module.exports = router;
